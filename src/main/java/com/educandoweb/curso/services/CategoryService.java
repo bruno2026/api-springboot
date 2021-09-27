502082b881fdf9987294c35eb7514c749a9e85e6
Bruno Gomes Ferreira <brunoo.gomes11@gmail.com>
2021-09-27 10:22:27 -0300

    Category

---
+++ src/main/java/com/educandoweb/curso/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curso.entities.Category;
+import com.educandoweb.curso.repositories.CategoryRepository;
+
+
+/*Anotation @service para registrar o component como um serviço!*/
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
